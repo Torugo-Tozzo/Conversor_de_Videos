c68123c3d168e34b109d5bfeff3681d83e18f198
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-05-27 13:07:32 -0300

    falta os botão de mp3e4

--- front/conversor/src/VideoForm.jsx
+++ front/conversor/src/VideoForm.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import logo from './Images/Conversor.png';
+import './styles.css';
 
 function VideoForm() {
   const [videoUrl, setVideoUrl] = useState('');
   const [videoTitle, setVideoTitle] = useState('');
   const [isDownloading, setIsDownloading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     try {
       const response = await axios.post('http://localhost:5000/processar', { videoUrl });
       setVideoTitle(response.data.title);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -28,25 +34,31 @@ function VideoForm() {
   };
 
   return (
-    <div>
+    <div className="video-form-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+
       <form onSubmit={handleSubmit} className="mb-3">
-        <div className="input-group">
+        <div className="input-bar">
           <input
             type="text"
             id="videoUrl"
             name="videoUrl"
             value={videoUrl}
             onChange={handleInputChange}
-            className="form-control"
+            className="form-control input-bar"
             placeholder="Insira a URL do vídeo"
             required
           />
-          <button type="submit" className="btn btn-primary">Buscar Título</button>
         </div>
+        <button type="submit" className="btn btn-primary">Buscar Título</button>
       </form>
 
-      {videoTitle && (
-        <div>
+      {isLoading && <p>Carregando...</p>}
+
+      {videoTitle && !isLoading && (
+        <div className="button-container">
           <h3>Título do Vídeo: {videoTitle}</h3>
           <button onClick={handleDownload} disabled={isDownloading} className="btn btn-primary">
             {isDownloading ? 'Fazendo download...' : 'Baixar'}
