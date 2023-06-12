eee6e9a10ad300e4e1609928e857809ce3a19c8a
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-06-12 13:42:17 -0300

    feito

--- front/conversor/src/FormatOptions.jsx
+++ front/conversor/src/FormatOptions.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import { FiMusic, FiFilm } from 'react-icons/fi';
+import './styles.css'; // Importe o arquivo CSS para estilização
 
 function FormatOptions({ selectedFormat, handleFormatChange }) {
   return (
     <div className="format-options">
-      <label>
+      <label className='space'>
+        <div className="icon-container">
+          <FiMusic className="icon" size={48} />
+          <span className="format-text">MP3</span>
+        </div>
+        
         <input
           type="radio"
           value="mp3"
           checked={selectedFormat === 'mp3'}
           onChange={handleFormatChange}
+          className="checkbox"
         />
-        MP3
       </label>
-      <label>
+      <label className='space'>
+        <div className="icon-container">
+          <FiFilm className="icon" size={48} />
+          <span className="format-text">MP4</span>
+        </div>
         <input
           type="radio"
           value="mp4"
           checked={selectedFormat === 'mp4'}
           onChange={handleFormatChange}
+          className="checkbox"
         />
-        MP4
       </label>
     </div>
   );
