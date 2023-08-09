ef745ee83f60779bb2df49d29c620905a1432559
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-08-09 12:40:18 -0300

    back only

--- front/conversor/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-      <footer className="footer">
-        <p>© 2023 Nome da Empresa. Todos os direitos reservados.</p>
-      </footer>
-    );
-  }
--- front/conversor/src/VideoTitle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function VideoTitle({ videoTitle }) {
-  return <h3>Título do Vídeo: {videoTitle}</h3>;
-}
-
-export default VideoTitle;
